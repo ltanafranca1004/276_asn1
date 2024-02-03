c95cbb6d37986e65d2eecf4d3bd70f693db9ec5f
Luis Tanafranca <80248146+ltanafranca1004@users.noreply.github.com>
2024-02-02 17:50:37 -0800

    Delete PycharmProjects directory

--- PycharmProjects/Test/Test 2.py
+++
@@ -1,5 +0,0 @@
-name = input("What's your name?")
-print("Hello" + name)
-
-birth_year = input("Enter your birth year: ")
-print("So your birthday is " + birth_year)
--- PycharmProjects/Test/Weight.py
+++
@@ -1,3 +0,0 @@
-weight_lb = input("Weight in pounds: ")
-weight_kg = int(weight_lb) / 2.205
-print("You weigh " + str(weight_kg) + " kg")
